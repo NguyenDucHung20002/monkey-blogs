1f7d665ad6ed700fc6f3faa01e174060f8144a39
unknown <manhha2392000@gmail.com>
2023-09-27 12:37:22 +0700

    setup server.js and .env.example

--- Server/config/env.js
+++ Server/config/env.js
@@ -2,4 +2,14 @@ require("dotenv").config();
 
 exports.env = {
   PORT: process.env.PORT,
+  MONGO_URL: process.env.MONGO_URL,
+  SECRET_KEY: process.env.SECRET_KEY,
+  EXPIRED_IN: process.env.EXPIRED_IN,
+  MONGO_BUCKET: process.env.MONGO_BUCKET,
+  MAIL_HOST: process.env.MAIL_HOST,
+  MAIL_PORT: process.env.MAIL_PORT,
+  MAIL_USERNAME: process.env.MAIL_USERNAME,
+  MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 };
--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,44 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
 const { env } = require("./config/env");
+const morgan = require("morgan");
+
+const createRoles = require("./constant/role");
 
 const app = express();
 const cors = require("cors");
+const authRouter = require("./routers/authRouter");
+
+require("./services/googleOauth");
+
+const { errorMiddleware } = require("./middlewares/errorMiddleware");
 
 app.use(cors());
+
+// express.json()
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+// passport
+app.use(passport.initialize());
+
+// Connet to database
+mongoose
+  .connect(env.MONGO_URL)
+  .then(() => console.log("connect success"))
+  .catch((err) => {
+    console.log("Can not connect database");
+    console.log(JSON.stringify(err, null, 2));
+  });
+
+// Create role
+createRoles();
+
+app.use("/api/auth", authRouter);
+
+app.use(errorMiddleware);
 
 app.listen(env.PORT || 8000, () => {
   console.log("Server is runing! port:" + env.PORT);
