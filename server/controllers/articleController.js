ff252a52123738ee293e785a940f9908a4a369fe
unknown <manhha2392000@gmail.com>
2023-10-02 09:41:42 +0700

    add create an article, update my article, delete my article, get an article. Fix utils toSlug, emailToUserName. Fix delete topic

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -1,6 +1,7 @@
 const toSlug = require("../utils/toSlug");
 const Profile = require("../models/Profile");
 const Article = require("../models/Article");
+const { removeFile } = require("../utils/removeFile");
 const { ErrorResponse } = require("../response/ErrorResponse");
 const { asyncMiddleware } = require("../middlewares/asyncMiddleware");
 
@@ -37,4 +38,93 @@ const createAnArticle = asyncMiddleware(async (req, res, next) => {
   });
 });
 
-module.exports = { createAnArticle };
+// update an article
+const updateMyArticle = asyncMiddleware(async (req, res, next) => {
+  const { id: user } = req.user;
+  const { slug } = req.params;
+  const { title, content, topics } = req.body;
+
+  const filename = req.file?.filename;
+
+  const profile = await Profile.findOne({ user });
+  if (!profile) {
+    throw new ErrorResponse(404, "profile not found");
+  }
+
+  const oldArticle = await Article.findOne({ author: profile._id, slug });
+  if (!oldArticle) {
+    throw new ErrorResponse(404, "article not found");
+  }
+
+  let updatedSlug;
+
+  if (title) {
+    updatedSlug = toSlug(title) + "-" + Date.now();
+  } else {
+    updatedSlug = toSlug(oldArticle.title) + "-" + Date.now();
+  }
+
+  await Article.findOneAndUpdate(
+    { author: profile._id, slug },
+    { title, slug: updatedSlug, img: filename, content, topics },
+    { new: true }
+  );
+
+  if (filename) {
+    removeFile(oldArticle.img);
+  }
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// delete an article
+const deleteMyArticle = asyncMiddleware(async (req, res, next) => {
+  const { id: user } = req.user;
+  const { slug } = req.params;
+
+  const profile = await Profile.findOne({ user });
+  if (!profile) {
+    throw new ErrorResponse(404, "profile not found");
+  }
+
+  const article = await Article.findOneAndDelete({ author: profile._id, slug });
+  if (article) {
+    removeFile(article.img);
+  }
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// get an article
+const getAnArticle = asyncMiddleware(async (req, res, next) => {
+  const { slug } = req.params;
+
+  const article = await Article.findOne({ slug })
+    .populate({
+      path: "author",
+      select: "avatar fullname username",
+    })
+    .populate({
+      path: "topics",
+      select: "name slug",
+    });
+  if (!article) {
+    throw new ErrorResponse(404, "article not found");
+  }
+
+  res.status(200).json({
+    success: true,
+    data: article,
+  });
+});
+
+module.exports = {
+  createAnArticle,
+  updateMyArticle,
+  getAnArticle,
+  deleteMyArticle,
+};
--- server/routers/articleRouter.js
+++ server/routers/articleRouter.js
@@ -15,4 +15,16 @@ router.post(
   articleController.createAnArticle
 );
 
+router.put(
+  "/:slug",
+  jwtAuth,
+  mongoUpload.single("img"),
+  validator(articleSchema.updateSchema),
+  articleController.updateMyArticle
+);
+
+router.delete("/:slug", jwtAuth, articleController.deleteMyArticle);
+
+router.get("/:slug", articleController.getAnArticle);
+
 module.exports = router;
