67f7802f90301711615c2d65a77394c7030b6afb
unknown <manhha2392000@gmail.com>
2023-10-12 15:23:20 +0700

    Delete server

--- server/validations/userSchema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("./joi");
-
-const updateSchema = Joi.object({
-  fullname: Joi.string().min(3).max(50),
-  bio: Joi.string().max(160),
-  about: Joi.string().max(200),
-});
-
-module.exports = { updateSchema };
