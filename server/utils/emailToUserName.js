81f58281564948fa1f577c29b85b1838fd27949d
hung duc <taoxinma01@gmail.com>
2023-10-12 14:33:37 +0700

    delete server

--- server/utils/emailToUserName.js
+++
@@ -1,9 +0,0 @@
-const emailToUserName = (email) => {
-  if (!email) {
-    return "";
-  }
-  const username = email.split("@");
-  return username[0];
-};
-
-module.exports = emailToUserName;
--- server/validations/profileSchema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("./joi");
-
-const updateSchema = Joi.object({
-  fullname: Joi.string().min(3).max(50),
-  bio: Joi.string().max(160).optional(),
-  about: Joi.string().max(200).optional(),
-});
-
-module.exports = { updateSchema };
