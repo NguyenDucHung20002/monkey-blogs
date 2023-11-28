e6c3a8f1e0ec0ca6d6bd96b4eb3a6f96f5add1fe
Hà Hoàng <manhha2392000@gmail.com>
2023-11-28 03:09:35 -0800

    Add get article likers

--- server/controllers/likeController.js
+++ server/controllers/likeController.js
@@ -2,6 +2,12 @@ import Like from "../models/mysql/Like.js";
 import asyncMiddleware from "../middlewares/asyncMiddleware.js";
 import ErrorResponse from "../responses/ErrorResponse.js";
 import Article from "../models/mysql/Article.js";
+import { Op } from "sequelize";
+import Profile from "../models/mysql/Profile.js";
+import User from "../models/mysql/User.js";
+import addUrlToImg from "../utils/addUrlToImg.js";
+import Block from "../models/mysql/Block.js";
+import Follow_Profile from "../models/mysql/Follow_Profile.js";
 
 // ==================== like an article ==================== //
 const likeAnArticle = asyncMiddleware(async (req, res, next) => {
@@ -54,4 +60,109 @@ const unLikeAnArticle = asyncMiddleware(async (req, res, next) => {
   res.json({ success: true, message: "Successfully unliked the article" });
 });
 
-export default { likeAnArticle, unLikeAnArticle };
+// ==================== get an article likers ==================== //
+const getArticleLiker = asyncMiddleware(async (req, res, next) => {
+  const me = req.me ? req.me : null;
+  const { skip = 0, limit = 15 } = req.query;
+  const { id } = req.params;
+
+  const article = await Article.findOne({
+    where: { id, status: "approved" },
+    attributes: ["id", "authorId"],
+  });
+
+  if (!article) throw ErrorResponse(404, "Article not found");
+
+  let likeProfiles = await Like.findAll({
+    where: { articleId: article.id, id: { [Op.gt]: skip } },
+    attributes: ["id"],
+    include: {
+      model: Profile,
+      as: "articleLike",
+      attributes: ["id", "fullname", "avatar", "bio"],
+      include: { model: User, as: "userInfo", attributes: ["username"] },
+    },
+    limit: Number(limit) ? Number(limit) : null,
+  });
+
+  let likers;
+
+  if (!me) {
+    likers = likeProfiles.map((likeProfile) => {
+      likeProfile.articleLike.avatar = addUrlToImg(
+        likeProfile.articleLike.avatar
+      );
+      return {
+        id: likeProfile.articleLike.id,
+        fullname: likeProfile.articleLike.fullname,
+        avatar: likeProfile.articleLike.avatar,
+        bio: likeProfile.articleLike.bio,
+        username: likeProfile.articleLike.userInfo.username,
+      };
+    });
+  }
+
+  if (me && me.profileInfo.id === article.authorId) {
+    likers = await Promise.all(
+      likeProfiles.map(async (likeProfile) => {
+        likeProfile.articleLike.avatar = addUrlToImg(
+          likeProfile.articleLike.avatar
+        );
+        return {
+          id: likeProfile.articleLike.id,
+          fullname: likeProfile.articleLike.fullname,
+          avatar: likeProfile.articleLike.avatar,
+          bio: likeProfile.articleLike.bio,
+          username: likeProfile.articleLike.userInfo.username,
+          isFollowed: !!(await Follow_Profile.findOne({
+            where: {
+              followedId: likeProfile.articleLike.id,
+              followerId: me.profileInfo.id,
+            },
+          })),
+        };
+      })
+    );
+  }
+
+  if (me && me.profileInfo.id !== article.authorId) {
+    likers = await Promise.all(
+      likeProfiles.map(async (likeProfile) => {
+        likeProfile.articleLike.avatar = addUrlToImg(
+          likeProfile.articleLike.avatar
+        );
+        return {
+          id: likeProfile.articleLike.id,
+          fullname: likeProfile.articleLike.fullname,
+          avatar: likeProfile.articleLike.avatar,
+          bio: likeProfile.articleLike.bio,
+          username: likeProfile.articleLike.userInfo.username,
+          isFollowed: !!(await Follow_Profile.findOne({
+            where: {
+              followedId: likeProfile.articleLike.id,
+              followerId: me.profileInfo.id,
+            },
+          })),
+          isBlocked: !!(await Block.findOne({
+            where: {
+              blockedId: likeProfile.articleLike.id,
+              blockerId: me.profileInfo.id,
+            },
+            attributes: ["id"],
+          })),
+        };
+      })
+    );
+  }
+
+  const newSkip =
+    likeProfiles.length > 0 ? likeProfiles[likeProfiles.length - 1].id : null;
+
+  res.json({
+    success: true,
+    data: likers,
+    newSkip,
+  });
+});
+
+export default { likeAnArticle, unLikeAnArticle, getArticleLiker };
--- server/routes/likeRoute.js
+++ server/routes/likeRoute.js
@@ -2,11 +2,14 @@ import express from "express";
 import requiredAuth from "../middlewares/requiredAuth.js";
 import likeController from "../controllers/likeController.js";
 import fetchMe from "../middlewares/fetchMe.js";
+import optionalAuth from "../middlewares/optionalAuth.js";
 
 const router = express.Router();
 
 router.post("/:id", requiredAuth, fetchMe, likeController.likeAnArticle);
 
 router.delete("/:id", requiredAuth, fetchMe, likeController.unLikeAnArticle);
+
+router.get("/:id", optionalAuth, fetchMe, likeController.getArticleLiker);
 
 export default router;
