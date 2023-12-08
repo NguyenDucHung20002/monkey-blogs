ec26d0d9b6ff8d4df8f85d23000fc09883a56181
Hà Hoàng <manhha2392000@gmail.com>
2023-12-08 19:24:59 +0700

    Fix ai check when uploaded  profile avatar

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -21,38 +21,12 @@ const upLoadAnImg = asyncMiddleware(async (req, res, next) => {
 
 // ==================== upload an avatar ==================== //
 const upLoadAnAvatar = asyncMiddleware(async (req, res, next) => {
-  const filename = req.file?.filename;
   const size = req.file?.size;
 
   const FILE_LIMIT = 15 * 1024 * 1024;
   if (size && size > FILE_LIMIT) {
     throw new ErrorResponse(400, "File too large");
   }
-
-  const gfs = MongoDB.gfs;
-
-  const readStream = gfs.openDownloadStreamByName(filename);
-  let chunks = [];
-
-  readStream.on("data", (chunk) => {
-    chunks.push(chunk);
-  });
-
-  readStream.on("end", () => {
-    const imgData = Buffer.concat(chunks).toString("base64");
-    clarifai(imgData, (err, results) => {
-      if (err) return res.status(500).json(err);
-
-      if (results[0].nsfw > 0.55) {
-        fileController.autoRemoveImg(filename);
-
-        throw ErrorResponse(
-          404,
-          "Sorry, but your image contains explicit content and is not allowed"
-        );
-      }
-    });
-  });
 
   res.status(201).json({
     success: true,
--- server/routes/fileRoute.js
+++ server/routes/fileRoute.js
@@ -3,6 +3,7 @@ import fileController from "../controllers/fileController.js";
 import requiredAuth from "../middlewares/requiredAuth.js";
 import fetchMe from "../middlewares/fetchMe.js";
 import mongoUpload from "../middlewares/mongoUpload.js";
+import checkUploadedImg from "../middlewares/checkUploadedImg.js";
 
 const router = express.Router();
 
@@ -19,6 +20,7 @@ router.post(
   requiredAuth,
   fetchMe,
   mongoUpload.single("avatar"),
+  checkUploadedImg,
   fileController.upLoadAnAvatar
 );
 
