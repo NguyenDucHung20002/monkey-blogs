86d19705c66e280e64baefc97e063d48bb348482
tinko0o <tinhneko2002@gmail.com>
2023-12-19 18:27:30 +0700

    loading update profile

--- client/src/components/form/UpdateProfile.jsx
+++ client/src/components/form/UpdateProfile.jsx
@@ -14,6 +14,7 @@ import Avatar from "../../modules/user/Avatar";
 const UpdateProfile = ({ show, setShow, user }) => {
   const [imageSrc, setImageSrc] = useState(null);
   const [showAnimation, setShowAnimation] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const token = localStorage.getItem("token");
   const options = {
     pauseOnHover: false,
@@ -44,8 +45,10 @@ const UpdateProfile = ({ show, setShow, user }) => {
     if (imageSrc?.avatar) {
       apiDeleteImage(imageSrc?.avatar);
     }
+    setIsLoading(true);
     const response = await apiUploadCheckImage(selectedFile);
     if (response?.data?.success) {
+      setIsLoading(false);
       const filename = response?.data?.filename;
       const imageUrl = `${config.SERVER_HOST}/file/${filename}`;
       setImageSrc({ imageUrl, avatar: filename });
@@ -195,8 +198,35 @@ const UpdateProfile = ({ show, setShow, user }) => {
                   <button
                     type="submit"
                     onClick={handleSubmit(onSubmitHandeler)}
-                    className="save"
+                    className={
+                      isLoading
+                        ? "save flex items-center cursor-not-allowed !bg-stone-200"
+                        : "save flex items-center"
+                    }
+                    disabled={isLoading}
                   >
+                    {isLoading && (
+                      <svg
+                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          strokeWidth="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                    )}
                     Save
                   </button>
                 </div>
