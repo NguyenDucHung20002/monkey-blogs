d59803a4655b59417ad70300d912dd2c68694f54
hung duc <taoxinma01@gmail.com>
2024-03-14 15:08:35 +0700

    update url server

--- client/src/utils/constants.jsx
+++ client/src/utils/constants.jsx
@@ -18,7 +18,7 @@ export const theme = {
 export const config = {
   CLIENT_PORT: 5173,
   CLIENT_HOST: "http://localhost",
-  SERVER_HOST: "http://178.128.21.84:8080/api/v1",
+  SERVER_HOST: "http://165.232.150.101:8080/api/v1",
   SERVER_URl: "http://localhost:8080",
 };
 
