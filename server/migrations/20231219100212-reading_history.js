989832bec70e05d893d52cd8e57071a4791bd123
Hà Hoàng <manhha2392000@gmail.com>
2024-03-21 15:58:21 +0700

    fix migration

--- server/migrations/20231219100212-reading_history.js
+++ server/migrations/20231219100212-reading_history.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("reading_historys", {
+    await queryInterface.createTable("reading_histories", {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
