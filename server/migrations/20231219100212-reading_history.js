a80ab34cbd3fcf98a431773855fb865ae83c7ea5
Hà Hoàng <manhha2392000@gmail.com>
2024-03-30 20:44:47 +0700

    fix migrate reading history

--- server/migrations/20231219100212-reading_history.js
+++ server/migrations/20231219100212-reading_history.js
@@ -41,6 +41,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("reading_historys");
+    await queryInterface.dropTable("reading_histories");
   },
 };
